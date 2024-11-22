c7f188a4b910a088c5ceb6c5aac26eb897177ed2
Emmanuels-code <arocket04@gmail.com>
2024-11-22 20:09:04 +0100

    wrote up converter screen

---
+++ lib/converterScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ConverterScreen extends StatelessWidget {
+  ConverterScreen({super.key});
+
+  Widget build(BuildContext) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          children: [Text('Png To Svg Converter')],
+        ),
+      ),
+    );
+  }
+}
