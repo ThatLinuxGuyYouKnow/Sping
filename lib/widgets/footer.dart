29e546892bc39309671ad90d0f7ef4d698d80d41
Emmanuels-code <arocket04@gmail.com>
2024-11-24 00:34:37 +0100

    update: updated footer widget

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Footer extends StatelessWidget {
   final BoxConstraints constraints;
@@ -8,6 +9,25 @@ class Footer extends StatelessWidget {
       color: Colors.black,
       width: constraints.maxWidth,
       height: 80,
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              'Made with ❤️ by',
+              style: GoogleFonts.ubuntu(color: Colors.white),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Text(
+              'ThatLinuxGuyYouKnow',
+              style: GoogleFonts.ubuntu(
+                  decoration: TextDecoration.underline, color: Colors.white),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
