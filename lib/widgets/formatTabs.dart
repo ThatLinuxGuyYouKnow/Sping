d0c9a225ca57812e7ed97ab885832542a72a931b
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-08-24 12:47:32 +0100

    feat: tabs for format conversion

--- lib/widgets/formatTabs.dart
+++ lib/widgets/formatTabs.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class FormatTab extends StatelessWidget {
-  FormatTab({super.key});
+  final String formatName;
+  FormatTab({super.key, required this.formatName});
 
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        height: 40,
+        decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.black,
+            ),
+            borderRadius: BorderRadius.circular(10)),
+        child: Text(
+          formatName,
+          style: GoogleFonts.ubuntu(),
+        ));
   }
 }
