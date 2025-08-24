f63e34edb3528ddc68fe3d6cc0b8e7b88e97f86d
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-08-24 13:45:38 +0100

    feat: restyled format tab

--- lib/widgets/formatTabs.dart
+++ lib/widgets/formatTabs.dart
@@ -3,19 +3,27 @@ import 'package:google_fonts/google_fonts.dart';
 
 class FormatTab extends StatelessWidget {
   final String formatName;
-  FormatTab({super.key, required this.formatName});
 
+  const FormatTab({super.key, required this.formatName});
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-        height: 40,
-        decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.black,
-            ),
-            borderRadius: BorderRadius.circular(10)),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.grey.shade400,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Center(
         child: Text(
           formatName,
-          style: GoogleFonts.ubuntu(),
-        ));
+          style: GoogleFonts.ubuntu(
+            fontWeight: FontWeight.w500,
+            color: Colors.black87,
+          ),
+        ),
+      ),
+    );
   }
 }
