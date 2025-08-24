ff085dd8631156d1e147cd7bf83c421533362d00
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-08-24 12:44:42 +0100

    feat: tabs for format conversion

---
+++ lib/widgets/formatTabs.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class FormatTab extends StatelessWidget {
+  FormatTab({super.key});
+
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
